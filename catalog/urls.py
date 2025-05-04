8e8b9fc196f4ec913218739565b7cf0a15596420
Pavel Artiev <arijev@bk.ru>
2025-05-04 16:27:35 +0300

    first commit

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-from catalog.views import home, contacts
+from catalog.views import home, contacts, products_list, product_detail
 
 app_name = 'catalog'
 
 
 urlpatterns = [
     path("home/", home, name="home"),
-    path("contacts/", contacts, name="contacts")
+    path("contacts/", contacts, name="contacts"),
+    path("products_list/", products_list, name="products_list"),
+    path("product_detail/<int:product_id>", product_detail, name="product_detail"),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Product
 
 # Create your views here.
 def home(request):
@@ -7,3 +8,13 @@ def home(request):
 
 def contacts(request):
     return render(request, "contacts.html")
+
+def products_list(request):
+    products = Product.objects.all()
+    context = {'products': products}
+    return render(request, 'products_list.html', context)
+
+def product_detail(request, product_id):
+    product = Product.objects.get(id=product_id)
+    context = {'product': product}
+    return render(request, 'product_detail.html', context)
