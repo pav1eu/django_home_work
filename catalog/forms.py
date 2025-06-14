946dffc80f1975c1aa09fd4b88647c3612404593
Pavel Artiev <arijev@bk.ru>
2025-06-14 21:44:08 +0300

    refactoring

--- catalog/forms.py
+++ catalog/forms.py
@@ -8,20 +8,20 @@ class ProductCreateForm(forms.ModelForm):
         fields = ['name', 'description', 'price', 'category']
 
     def clean_name(self):
-        cleaned_data = super().clean()
         name = self.cleaned_data.get('name')
-        for word in settings.BAD_WORDS:
-            if word.lower() in name.lower():
-                raise forms.ValidationError(f"Имя содержит запрещённое слово: '{word}'")
-        return cleaned_data
+        if name:
+            for word in settings.BAD_WORDS:
+                if word.lower() in name.lower():
+                    raise forms.ValidationError(f"Имя содержит запрещённое слово: '{word}'")
+        return name
 
     def clean_description(self):
-        cleaned_data = super().clean()
         description = self.cleaned_data.get('description')
-        for word in settings.BAD_WORDS:
-            if word.lower() in description.lower():
-                raise forms.ValidationError(f"Описание содержит запрещённое слово: '{word}'")
-        return cleaned_data
+        if description:
+            for word in settings.BAD_WORDS:
+                if word.lower() in description.lower():
+                    raise forms.ValidationError(f"Описание содержит запрещённое слово: '{word}'")
+        return description
 
     def clean_price(self):
         price = self.cleaned_data.get('price')
@@ -29,26 +29,19 @@ class ProductCreateForm(forms.ModelForm):
             raise forms.ValidationError("Цена не может быть отрицательной.")
         return price
 
-
     def __init__(self, *args, **kwargs):
         super(ProductCreateForm, self).__init__(*args, **kwargs)
-
         self.fields['name'].widget.attrs.update({
             'class': 'form-control',
             'placeholder': 'Введите название'
         })
-
         self.fields['description'].widget.attrs.update({
             'class': 'form-control',
-            'placeholder': 'Блаблабла'
+            'placeholder': 'Описание товара'
         })
-
         self.fields['price'].widget.attrs.update({
             'class': 'form-control',
         })
-
         self.fields['category'].widget.attrs.update({
             'class': 'form-control',
         })
-
-
